7e33e7433fecc8fb6d14de6d44724c594481019b
nimit <nimitkhandelwal7@gmail.com>
2015-09-13 08:13:03 +0530

    article ListView created(yearwise and monthwise)

--- news/views.py
+++ news/views.py
@@ -9,20 +9,15 @@ from django.shortcuts import render_to_response
 class ArticlesList(ListView):
     model=Articles
     template_name='articles_list.html'
+    def get_queryset(self):
+        if self.kwargs['month'] and self.kwargs['year']:
+            return self.model.objects.filter(timestamp__month=self.kwargs['month'],timestamp__year=self.kwargs['year'])
+        elif self.kwargs['year']:
+            return self.model.objects.filter(timestamp__year=self.kwargs['year'])
+        else:
+            return self.model.objects.all()
     def get_context_data(self, **kwargs):
         context=super(ArticlesList,self).get_context_data(**kwargs)
-        year=self.kwargs.get('year', None)
-        month=self.kwargs.get('month',None)
-        if month and year:
-            date_list=Articles.objects.filter(timestamp__month=month,timestamp__year=year)
-        elif year:
-            date_list=Articles.objects.filter(timestamp__year=year)
-        else:
-            date_list=Articles.objects.all()
-        #for years in date_list:
-            #context['list']+=Articles.objects.filter(timestamp__year=years.year)
-        resultset=date_list
-        results = [ob.as_article() for ob in resultset]
-        context['list']=json.dumps(results)
-
         return context
+    class ArticleDetail(DetailView):
+        model=Articles
